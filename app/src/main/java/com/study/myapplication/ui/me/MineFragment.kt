041db39b9530a9ddae66a4c63c1ec57fcdd849f9
suxiting <xitingsu@jieyuechina.com>
2019-11-11 20:08:06 +0800

    33

---
+++ app/src/main/java/com/study/myapplication/ui/me/MineFragment.kt
@@ -0,0 +1,11 @@
+package com.study.myapplication.ui.me
+
+
+import com.study.myapplication.R
+import com.study.myapplication.base.BaseFragment
+import com.study.myapplication.databinding.FragmentMineBinding
+
+class MineFragment : BaseFragment<FragmentMineBinding>() {
+    override val layoutId: Int
+        get() = R.layout.fragment_mine
+}
